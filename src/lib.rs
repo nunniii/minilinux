393497e2b35700ae15e95e43e91ea5adcc5f3a0b
Mateus Nunes <mateusnss@protonmail.ch>
2024-11-15 17:48:43 -0300

    i

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,44 @@
 use wasm_bindgen::prelude::*;
 
-// Função de teste
+
 #[wasm_bindgen]
 pub fn get_message() -> String {
-    "Hello from minilinux!".to_string()
+    "Hello from MiniLinux!".to_string()
 }
 
-// Função que simula a execução de comandos
+
+#[wasm_bindgen]
+pub fn help() -> String {
+    "Available commands: help, version, hello".to_string()
+}
+
+
+#[wasm_bindgen]
+pub fn version() -> String {
+    "MiniLinux version 1.0.0".to_string()
+}
+
+
+#[wasm_bindgen]
+pub fn hello(s: String) -> String {
+    format!("Test called:: {}. \n{}", s, get_message())
+}
+
+// Função principal para execução de comandos
 #[wasm_bindgen]
 pub fn call_function(command: String) -> String {
-    match command.as_str() {
-        "hello" => "Hello, World!".to_string(),
-        "version" => "Minilinux v1.0".to_string(),
-        "help" => "Available commands: hello, version, help".to_string(),
+    let parts: Vec<&str> = command.split_whitespace().collect();
+
+    match parts.get(0).unwrap_or(&"") {
+        &"help" => help(),
+        &"version" => version(),
+        &"hello" => {
+            if parts.len() > 1 {
+                hello(parts[1].to_string())
+            } else {
+                "Usage: hello <some string>".to_string()
+            }
+        }
         _ => "Command not found.".to_string(),
     }
 }
